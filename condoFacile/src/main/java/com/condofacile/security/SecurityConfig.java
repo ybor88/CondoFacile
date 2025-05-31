93b3c223669d3b77f17736df89b694f9048b9004
DIR\roberto.di.flumeri <paolo.fusi@consulenti.gruppohera.it>
2025-05-31 15:39:38 +0200

    add login page and layout

---
+++ condoFacile/src/main/java/com/condofacile/security/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.condofacile.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.context.annotation.Bean;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .csrf(AbstractHttpConfigurer::disable);  // nuova sintassi
+        return http.build();
+    }
+}
