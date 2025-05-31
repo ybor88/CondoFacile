6787111777c4142b5a94769bf77dfc79c99c4c49
DIR\roberto.di.flumeri <paolo.fusi@consulenti.gruppohera.it>
2025-05-31 15:15:16 +0200

    add login page and layout

---
+++ condoFacile/src/main/java/com/condofacile/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.condofacile.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/Condofacile")
+    public String home() {
+        return "home"; // Rito-rna home.html da templates
+    }
+}
